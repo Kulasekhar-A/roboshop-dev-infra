da5ea889c0d76531cb5e05c68350d7f13c763bb3
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-03-06 19:24:08 +0530

    actions

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -9,4 +9,5 @@ cd /home/ec2-user
 git clone https://github.com/Kulasekhar-A/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
+git pull
 ansible-playbook -e component=$component roboshop.yaml
