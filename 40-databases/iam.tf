d378c8c6072f8ab9fa792c660a5df316ef2561e1
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-03-07 18:28:29 +0530

    actions

---
+++ 40-databases/iam.tf
@@ -0,0 +1,70 @@
+resource "aws_iam_role" "mysql" {
+  name = local.mysql_role_name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge (
+    {
+        Name = local.mysql_role_name
+    },
+    local.common_tags
+  )
+}  
+
+resource "aws_iam_policy" "mysql" {
+  name        = local.mysql_policy_name
+  description = "A policy for MySQL EC2 instance"
+  policy      = file("mysql-iam-policy.json")
+}
+
+resource "aws_iam_role_policy_attachment" "mysql" {
+  role       = aws_iam_role.mysql.name
+  policy_arn = aws_iam_policy.mysql.arn
+}
+
+resource "aws_iam_instance_profile" "mysql" {
+  name = "${var.project}-${var.environment}-mysql"
+  role = aws_iam_role.mysql.name
+}
+
+
+resource "aws_iam_role" "rabbitmq" {
+  name = local.rabbitmq_role_name
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge (
+    {
+        Name = local.rabbitmq_role_name
+    },
+    local.common_tags
+  )
+}  
