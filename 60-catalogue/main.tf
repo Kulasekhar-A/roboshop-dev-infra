d14933aec9d669b2b3aa5041f652fb5a941f007b
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-03-12 15:57:37 +0530

    actions

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -173,6 +173,7 @@ resource "aws_autoscaling_policy" "catalogue" {
   autoscaling_group_name = aws_autoscaling_group.catalogue.name
   name                   = "${var.project}-${var.environment}-catalogue"
   policy_type            = "TargetTrackingScaling"
+  estimated_instance_warmup = 120
 
   
   target_tracking_configuration {
