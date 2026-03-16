378c322552f72a35aa717174a6a06fd06f27b5d8
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-03-16 22:23:16 +0530

    actions

---
+++ 70-acm/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+    type=string
+    default="roboshop"
+}
+
+variable "environment" {
+    type=string
+    default="dev"
+}
+
+variable "zone_id" {
+    default = "Z02888033TNMXI6HSFQ0T"
+}
+
+variable "domain_name" {
+    default = "annuru.online"
+}
---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z02888033TNMXI6HSFQ0T"
+}
+
+variable "domain_name" {
+    default = "annuru.online"
+}
